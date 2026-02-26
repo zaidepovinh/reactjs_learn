fa6e7327bbcdcba9da0fbd991a4d55cfaa70a130
zaidepovinh <issproxsv4@gmail.com>
2026-02-26 18:07:59 +0700

    video 27

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
+
+const myFunction = () => {
+  console.log("hello world");
+}
+
+myFunction();
 
   return (
     <>
